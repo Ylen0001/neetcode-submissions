5bed92c923014f96cf5dded26341279e8643e85e
Yoann Lenoel <yoann.lenoel@gmail.com>
2026-05-21 14:47:44 +0200

    Add: top-k-elements-in-list - submission-14

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-14.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        const mapped = new Map();
+        for(let i = 0; i < nums.length; i++){
+            mapped.set(nums[i], (mapped.get(nums[i]) | 0) + 1)
+        }
+        let res = Array.from(mapped.entries()).sort((a, b) => b[1] - a[1]);
+        res = res.slice(0, k).map(a => a[0]);
+        return res;
+    }
+}
