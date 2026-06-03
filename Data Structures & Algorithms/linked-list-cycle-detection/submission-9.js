a127de60d439f4dbd14aa1381502c3283b277caf
Yoann Lenoel <yoann.lenoel@gmail.com>
2026-06-03 14:59:13 +0200

    Add: linked-list-cycle-detection - submission-9

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-9.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {boolean}
+     */
+    hasCycle(head) {
+        if(!head)
+            return false;
+        let fast = head.next;
+        let slow = head;
+
+        while(fast && fast.next){
+            if(fast === slow)
+                return true;
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return false;
+    }
+}
