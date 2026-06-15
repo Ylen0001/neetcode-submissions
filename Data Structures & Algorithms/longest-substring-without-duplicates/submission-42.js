21822b229f54c761ba85fe242443ca714d91428e
Yoann Lenoel <yoann.lenoel@gmail.com>
2026-06-15 21:35:41 +0200

    Add: longest-substring-without-duplicates - submission-42

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-42.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s) {
+        let mapped = new Map();
+        let l = 0;
+        let res = 0;
+        for(let r = 0; r < s.length; r++){
+            if(mapped.has(s[r]) && mapped.get(s[r]) >= l){
+                l = Math.max(l, mapped.get(s[r]));
+                l++;
+            }
+            mapped.set(s[r], r);
+            res = Math.max(res, r - l + 1);
+        }
+        return res;
+    }
+}
