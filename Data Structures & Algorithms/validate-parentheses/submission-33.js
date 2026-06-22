fa4bf0aa1f4a1f7de2cc8fe482650120331d3e2e
Yoann Lenoel <yoann.lenoel@gmail.com>
2026-06-22 19:38:11 +0200

    Add: validate-parentheses - submission-33

---
+++ Data Structures & Algorithms/validate-parentheses/submission-33.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        const pairs = {
+            ')':'(',
+            ']':'[',
+            '}':'{'
+        }
+        let stack = [];
+        for(let i = 0; i < s.length; i++){
+            if(s[i] === '(' || s[i] === '[' || s[i] === '{'){
+                stack.push(s[i]);
+            } else if(stack.pop() !== pairs[s[i]])
+                return false;
+        }
+        return stack.length === 0;
+    }
+}
